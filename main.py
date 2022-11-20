b7f43e5be9b986a6edaba1bccae23d82a16214d8
Olli <olli.f44@googlemail.com>
2022-11-20 11:30:18 +0100

    streamlit_commit

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("Hello world")
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+class Plant_sensor:
+
+    def __init__(self):
+        self.df = pd.DataFrame(
+            columns=["Datum","Temperatur","Feuchtigkeit","Licht","Leitfähigkeit"]
+        )
+
+    def add_entry(self, datum, temp, feuchtigkeit, licht, leitfaehigkeit):
+        new_row = [datum,temp,feuchtigkeit,licht,leitfaehigkeit]
+        self.df.loc[len(self.df)] = new_row
+
+    def show(self):
+        st.write(self.df)
+
+st.write("Hello *World!* :sunglasses:")
+
+ps = Plant_sensor()
+
+date = datetime.now().strftime("%c")
+temperatur = str(22)
+feuchtigkeit = str(18)
+licht = str(5)
+leitfaehigkeit = str(50)
+
+ps.add_entry(date,temperatur,feuchtigkeit,licht,leitfaehigkeit)
+ps.show()
+
